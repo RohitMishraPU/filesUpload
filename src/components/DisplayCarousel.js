7aa4e1daab12382dce7df2ba58b35cb9e5aeb969
Rohit Mishra <roromish@oradev.oraclecorp.com>
2021-12-29 19:07:42 +0530

    Second Commit

--- src/components/DisplayCarousel.js
+++ src/components/DisplayCarousel.js
@@ -8,6 +8,28 @@ import ANYImage from '../assets/any.jpg'
 import ANYIcon from '../assets/zip.png'
 import useFirestore from '../DataHooks/useFireStore'
 
+
+const setDisplayData = (data) => {
+    const dateFormat = JSON.stringify(data.createdAt.toDate().toDateString()).replace(/['"]+/g, '')
+    const current = {
+        name: data.fileName,
+        type : data.fileType,
+        date : dateFormat
+    }
+    if(current.type == 'IMAGE'){
+        current.imageData = { base : data.url, icon : IMAGEIcon, url:data.url};
+    }else if(current.type == 'WORD'){
+        current.imageData = { base : DOCImage, icon : DOCIcon,  url:data.url};
+    }else if(current.type == 'PDF'){
+        current.imageData = { base : PDFImage, icon : PDFIcon,  url:data.url};
+    }else if(current.type == 'ANY'){
+        current.imageData = { base : ANYImage, icon : ANYIcon, url:data.url};
+    }
+
+    return current;
+
+}
+
 function DisplayCarousel() {
     const { docs } = useFirestore('PICS');
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -16,70 +38,32 @@ function DisplayCarousel() {
 
     useEffect(() => {
         if(docs.length){
-            console.log(docs);
-            const temp = docs[currentIndex];
-            const dateFormat = JSON.stringify(temp.createdAt.toDate().toDateString()).replace(/['"]+/g, '')
-            const current = {
-                name: temp.fileName,
-                type : temp.fileType,
-                date :  dateFormat
-            }
-            if(current.type == 'IMAGE'){
-                setCurrentImage({ base : temp.url, icon : IMAGEIcon});
-            }else if(current.type == 'WORD'){
-                setCurrentImage({ base : DOCImage, icon : DOCIcon});
-            }else if(current.type == 'PDF'){
-                setCurrentImage({ base : PDFImage, icon : PDFIcon});
-            }else if(current.type == 'ANY'){
-                setCurrentImage({ base : ANYImage, icon : ANYIcon});
-            }
+            const current =  setDisplayData(docs[currentIndex]);
+            
             setData(current);
+            setCurrentImage(current.imageData);
         }
        
     }, [docs])
 
     const setPreviousIndex = () => {
         if(currentIndex == 0) return;
+
         setCurrentIndex( lastIndex => lastIndex -1);
-            const temp = docs[currentIndex -1];            
-            const dateFormat = JSON.stringify(temp.createdAt.toDate().toDateString()).replace(/['"]+/g, '')
-            const current = {
-                name: temp.fileName,
-                type : temp.fileType,
-                date : dateFormat
-            }
-            if(current.type == 'IMAGE'){
-                setCurrentImage({ base : temp.url, icon : IMAGEIcon});
-            }else if(current.type == 'WORD'){
-                setCurrentImage({ base : DOCImage, icon : DOCIcon});
-            }else if(current.type == 'PDF'){
-                setCurrentImage({ base : PDFImage, icon : PDFIcon});
-            }else if(current.type == 'ANY'){
-                setCurrentImage({ base : ANYImage, icon : ANYIcon});
-            }
-            setData(current);
+        const current =  setDisplayData(docs[currentIndex -1]);
+            
+        setData(current);
+        setCurrentImage(current.imageData);
     }
 
     const setNextIndex = () => {
         if(currentIndex == docs.length -1 ) return;
+
         setCurrentIndex( lastIndex => lastIndex + 1);
-        const temp = docs[currentIndex +1];                
-        const dateFormat = JSON.stringify(temp.createdAt.toDate().toDateString()).replace(/['"]+/g, '')
-            const current = {
-                name: temp.fileName,
-                type : temp.fileType,
-                date : dateFormat
-            }
-            if(current.type == 'IMAGE'){
-                setCurrentImage({ base : temp.url, icon : IMAGEIcon, url:temp.url});
-            }else if(current.type == 'WORD'){
-                setCurrentImage({ base : DOCImage, icon : DOCIcon,  url:temp.url});
-            }else if(current.type == 'PDF'){
-                setCurrentImage({ base : PDFImage, icon : PDFIcon,  url:temp.url});
-            }else if(current.type == 'ANY'){
-                setCurrentImage({ base : ANYImage, icon : ANYIcon, url:temp.url});
-            }
-            setData(current);
+        const current =  setDisplayData(docs[currentIndex + 1]);
+            
+        setData(current);
+        setCurrentImage(current.imageData);
     }
     
     
@@ -92,8 +76,7 @@ function DisplayCarousel() {
                 docs.length ?  <>
                 <a  href={currentImage.url} target="_blank">
                 <img className = {(data.type == 'IMAGE') ? 'imageFile' : 'otherFile'} src={currentImage.base} alt="" /> 
-                </a>
-               
+                </a>             
 
                 
                 <button className="prev" onClick={(e) => setPreviousIndex()}>Prev</button>
